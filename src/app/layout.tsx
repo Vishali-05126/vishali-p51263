ad30bacac27a51d8be022556d986fe8b22b886f9
Vishali .P <vaishuvishali55@gmail.com>
2026-01-31 20:33:37 +0000

    change the uiux attractivelybits very simple

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className="light">
+    <html lang="en" suppressHydrationWarning className="dark">
       <body className={cn("font-body antialiased", fontBody.variable, fontHeadline.variable)}>
         {children}
         <Toaster />
